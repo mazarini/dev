6630e327d7741b5678875f71654e4fd983f667f1
mazarini <mazarini@protonmail.com>
2019-12-07 19:19:25 +0100

    Add bootstrap-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2019 Mazarini <mazarini@protonmail.com>.
- * This file is part of mazarini/dev.
- *
- * mazarini/dev is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * mazarini/dev is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
@@ -31,4 +14,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Mazarini\BootstrapBundle\MazariniBootstrapBundle::class => ['all' => true],
 ];
