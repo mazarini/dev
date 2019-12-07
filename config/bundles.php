e910733a4a817a8cd72c61e51c05c18a9d350388
mazarini <mazarini@protonmail.com>
2019-12-07 13:48:46 +0100

    First CRUD

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,22 @@
 <?php
 
+/*
+ * Copyright (C) 2019 Mazarini <mazarini@protonmail.com>.
+ * This file is part of mazarini/dev.
+ *
+ * mazarini/dev is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * mazarini/dev is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
@@ -8,7 +25,10 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Mazarini\PackageBundle\MazariniPackageBundle::class => ['dev' => true, 'test' => true],
-    Mazarini\ToolsBundle\MazariniToolsBundle::class => ['dev' => true, 'test' => true],
+    Mazarini\PackageBundle\MazariniPackageBundle::class => ['dev' => true],
+    Mazarini\ToolsBundle\MazariniToolsBundle::class => ['all' => true],
     Mazarini\TestBundle\MazariniTestBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
