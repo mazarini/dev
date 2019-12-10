8316183e3dcd1c0a1656bdc4eb9c9e0aaff101d3
mazarini <mazarini@protonmail.com>
2019-12-10 09:03:24 +0100

    Add User

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Mazarini\BootstrapBundle\MazariniBootstrapBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
