8fc67846d25dcdef84822a029b9ca16ab60f11a6
Mazarini <mazarini@mazarini.ovh>
2020-12-27 15:47:03 +0100

    Run php-cs-fixer

--- component/tools-bundle/tests/Data/LinksTest.php
+++ component/tools-bundle/tests/Data/LinksTest.php
@@ -2,14 +2,14 @@
 
 /*
  * Copyright (C) 2019-2020 Mazarini <mazarini@protonmail.com>.
- * This file is part of mazarini/tools-bundle.
+ * This file is part of mazarini/dev.
  *
- * mazarini/tools-bundle is free software: you can redistribute it and/or
+ * mazarini/dev is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * mazarini/tools-bundle is distributed in the hope that it will be useful,
+ * mazarini/dev is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
@@ -30,7 +30,7 @@ class LinksTest extends KernelTestCase
      */
     protected $links;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         $this->links = new Links('/active');
         $this->links->addLink(new Link('disable', '#'));
