444e43fe1b56bd410507235f1ead1f91434935e8
Mazarini <mazarini@protonmail.com>
2022-01-23 14:16:37 +0100

    Start project

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the mazarini/dev project.
+ *
+ * (c) Mazarini <mazarini@protonmail.com.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'index')]
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
---
+++ tests/Controller/IndexTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the mazarini/dev project.
+ *
+ * (c) Mazarini <mazarini@protonmail.com.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello IndexController! ✅');
+    }
+}
