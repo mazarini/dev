0977ca19bf7205fee4123410bfef87869d4fb179
Mazarini <mazarini@protonmail.com>
2022-01-26 08:50:39 +0100

    Add easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the mazarini/dev project.
+ *
+ * (c) Mazarini <mazarini@protonmail.com.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin/dashboard', name: 'admin_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            // the name visible to end users
+            ->setTitle('ACME Corp.')
+            // you can include HTML contents too (e.g. to link to an image)
+            ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
+
+            // the path defined in this method is passed to the Twig asset() function
+            ->setFaviconPath('favicon.svg')
+
+            // the domain used by default is 'messages'
+            ->setTranslationDomain('my-custom-domain')
+
+            // there's no need to define the "text direction" explicitly because
+            // its default value is inferred dynamically from the user locale
+            ->setTextDirection('ltr')
+
+            // set this option if you prefer the page content to span the entire
+            // browser width, instead of the default design which sets a max width
+            ->renderContentMaximized()
+
+            // set this option if you prefer the sidebar (which contains the main menu)
+            // to be displayed as a narrow column instead of the default expanded design
+            ->renderSidebarMinimized()
+
+            // by default, all backend URLs include a signature hash. If a user changes any
+            // query parameter (to "hack" the backend) the signature won't match and EasyAdmin
+            // triggers an error. If this causes any issue in your backend, call this method
+            // to disable this feature and remove all URL signature checks
+            ->disableUrlSignatures()
+
+            // by default, all backend URLs are generated as absolute URLs. If you
+            // need to generate relative URLs instead, call this method
+            ->generateRelativeUrls()
+        ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+    }
+}
