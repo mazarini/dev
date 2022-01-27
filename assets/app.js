63c93fd0af963bfb311c9832c437ef744f906d7c
Mazarini <mazarini@protonmail.com>
2022-01-27 18:54:00 +0100

    Fix asset / unknown $

--- assets/app.js
+++ assets/app.js
@@ -9,9 +9,11 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+require('bootstrap/js/dist/popover');
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
